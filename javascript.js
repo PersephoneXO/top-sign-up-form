bcbfa747f4f3a4e50402d1335adadeead81e2f61
Persephone <134444980+PersephoneXO@users.noreply.github.com>
2023-12-03 15:01:09 -0500

    javascript finished

--- javascript.js
+++ javascript.js
@@ -1,8 +1,29 @@
-function validatePsw(){
-    let pswd=document.getElementById('password');
-    let confirmPswd=document.getElementById('confirm-p');
+const button=document.getElementById('create');
+button.disabled=true;
+
+
+document.getElementById('confirm-p').oninput=function(){
+    var pswd=document.getElementById('password').value;
+    var confirmPswd=document.getElementById('confirm-p').value;
 
     if(pswd!=confirmPswd){
-
+        document.getElementById('password').style.borderColor='red';
+        document.getElementById('confirm-p').style.borderColor='red';
+        document.getElementById('pswd-message').innerHTML='* Passwords do not match';
+        button.disabled=true;
+        return false;
+    }
+    else{
+        document.getElementById('pswd-message').innerHTML='';
+        document.getElementById('password').style.borderColor='green';
+        document.getElementById('confirm-p').style.borderColor='green';
+        button.disabled=false;
+        return true;
     }
 }
+
+
+button.addEventListener('click',(event)=>{
+    event.preventDefault();
+    document.getElementById('successful-creation').innerHTML='Account Created Successfully';
+});
